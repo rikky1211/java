341de87919044af0931b13068405aac3c0da10b9
Ryuki Isami <isamin1211.program@gmail.com>
2025-12-22 12:03:26 +0900

    20251222_r_chapter06s

---
+++ R_06_CalcLogic.java
@@ -0,0 +1,9 @@
+public class R_06_CalcLogic{
+  public static int tasu(int a, int b) {
+    return a + b ;
+  }
+
+  public static int hiku(int a, int b) {
+    return a - b ;
+  }
+}
---
+++ R_06_main.java
@@ -0,0 +1,10 @@
+public class R_06_main{
+  public static void main(String[] args){
+    int a = 20, b = 10;
+
+    int total = R_06_CalcLogic.tasu(a, b);
+    System.out.println(total);
+    int delta = R_06_CalcLogic.hiku(a, b);
+    System.out.println(delta);
+  }
+}
